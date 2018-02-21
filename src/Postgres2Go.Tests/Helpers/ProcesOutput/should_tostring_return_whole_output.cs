8453203d506ec354091bd2c9d85695ff5691704b
Rafal Zabrowarny <zabrowarnyrafal@users.noreply.github.com>
2018-02-21 23:06:36 +0100

    #35 - ProcesOutput type new method ToString() concatenates standard and error output; Postgres process when throws exception then includes whole output of pg_ctl

---
+++ src/Postgres2Go.Tests/Helpers/ProcesOutput/should_tostring_return_whole_output.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace Postgres2Go.Tests.Helpers.ProcesOutput
+{
+    public class should_tostring_return_whole_output
+    {
+        [Theory]
+        [InlineData("only stand output",null)]
+        [InlineData(null,"only err output")]
+        [InlineData("stand output","err output")]
+        public void run(string standardOutput, string errorOutput)
+        {
+            // PREPARE
+            var collectionOfstandardOutputs = new List<string>{"STD OUTPUT:", standardOutput};
+            var collectionOfErrorOutputs = new List<string>{"ERR OUTPUT:", errorOutput};
+
+            var sut = new Helper.Process.ProcessOutput(collectionOfstandardOutputs, collectionOfErrorOutputs, 0);
+
+            // RUN
+            string wholeFormatedOutput = sut.ToString();
+
+            // ASSERT
+            wholeFormatedOutput
+                .Should()
+                .Be(String.Format(@"
+===
+STD OUTPUT:
+{0}
+
+ERR OUTPUT:
+{1}
+"
+                , standardOutput, errorOutput));
+
+
+        }
+    }
+}
---
+++ src/Postgres2Go/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Postgres2Go.Tests")]
+[assembly: AssemblyVersion("0.3.0.0")]
+[assembly: AssemblyInformationalVersion("0.3.0-35-print-whole-pg-output.1+1.Branch.feature/35-print-whole-pg-output.Sha.ecedced0a29007f1835f0d783fb83a4e990fbbc6")]
+[assembly: AssemblyFileVersion("0.3.0.0")]
