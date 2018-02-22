548402357820730450b7f878d83b0fe981adff7b
Rafal Zabrowarny <zabrowarnyrafal@users.noreply.github.com>
2018-02-22 19:53:54 +0100

    #35 - fix: broken unit tests due improper usage of white chars

--- src/Postgres2Go.Tests/Helpers/ProcesOutput/procesoutput_toString.cs
+++ src/Postgres2Go.Tests/Helpers/ProcesOutput/procesoutput_toString.cs
@@ -5,13 +5,13 @@ using Xunit;
 
 namespace Postgres2Go.Tests.Helpers.ProcesOutput
 {
-    public class should_tostring_return_whole_output
+    public class procesoutput_toString
     {
         [Theory]
         [InlineData("only stand output",null)]
         [InlineData(null,"only err output")]
         [InlineData("stand output","err output")]
-        public void run(string standardOutput, string errorOutput)
+        public void should_return_concatenation_of_outputs(string standardOutput, string errorOutput)
         {
             // PREPARE
             var collectionOfstandardOutputs = new List<string>{"STD OUTPUT:", standardOutput};
@@ -23,18 +23,14 @@ namespace Postgres2Go.Tests.Helpers.ProcesOutput
             string wholeFormatedOutput = sut.ToString();
 
             // ASSERT
-            wholeFormatedOutput
-                .Should()
-                .Be(String.Format(@"
-===
-STD OUTPUT:
-{0}
+            
+            string expectation = $"{Environment.NewLine}" +
+                                 $"==={Environment.NewLine}" +
+                                 $"STD OUTPUT:{Environment.NewLine}{standardOutput}{Environment.NewLine}" +
+                                 $"{Environment.NewLine}" +
+                                 $"ERR OUTPUT:{Environment.NewLine}{errorOutput}{Environment.NewLine}";
 
-ERR OUTPUT:
-{1}
-"
-                , standardOutput, errorOutput));
-
+            wholeFormatedOutput.Should().Be(expectation);
 
         }
     }
