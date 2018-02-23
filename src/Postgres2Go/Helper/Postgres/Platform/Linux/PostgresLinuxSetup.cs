a9bbc095db9e6fff7679490cbe741e720424ff10
Rafał Zabrowarny <zabrowarnyrafal@users.noreply.github.com>
2018-02-23 22:41:39 +0100

    #36 fix: linux pg setup includes recreation of symbolic links in distro lib/ folder

---
+++ src/Postgres2Go/Helper/Postgres/Platform/Linux/PostgresLinuxSetup.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+
+namespace Postgres2Go.Helper.Postgres.Platform.Linux
+{
+    internal class PostgresLinuxSetup
+    {
+        internal void Start(string pgBinDirectoryPath)
+        {
+            GrantPostgresExecutablePermission(pgBinDirectoryPath);
+            PreparePostgresLibrarySymbolicLinks(pgBinDirectoryPath);
+        }
+
+        private void GrantPostgresExecutablePermission(string pgBinDirectoryPath)
+        {
+            FileSystem.FileSystem.GrantExecutablePermission(Path.Combine(pgBinDirectoryPath, PostgresDefaults.ServerControllerExecutable));
+            FileSystem.FileSystem.GrantExecutablePermission(Path.Combine(pgBinDirectoryPath, PostgresDefaults.ServerExecutable));
+            FileSystem.FileSystem.GrantExecutablePermission(Path.Combine(pgBinDirectoryPath, PostgresDefaults.ServerInitializatorExecutable));
+        }
+
+        private void PreparePostgresLibrarySymbolicLinks(string pgBinDirectoryPath)
+        {
+            PostgresLibLinksFixerProcess.Exec(pgBinDirectoryPath);
+        }
+    }
+}
