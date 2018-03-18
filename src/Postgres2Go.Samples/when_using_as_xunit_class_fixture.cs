5abab14418107f0ea0d2e58608f053897cb7eff0
Rafal Zabrowarny <zabrowarnyrafal@users.noreply.github.com>
2018-03-18 21:30:03 +0100

    #8 added nunit test sample

---
+++ src/Postgres2Go.Samples/when_using_as_xunit_class_fixture.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+using Xunit;
+
+namespace Postgres2Go.Samples
+{
+    public class PgFixture : IDisposable
+    {
+        private readonly PostgresRunner _pgRunner;
+
+        public PgFixture()
+        {
+            _pgRunner = PostgresRunner.Start();
+        }
+
+        public void Dispose() => _pgRunner?.Dispose();
+
+        public string ConnectionString => _pgRunner.GetConnectionString();
+    }
+    
+    public class when_using_as_xunit_class_fixture : IClassFixture<PgFixture>
+    {
+        private readonly PgFixture _fixture;
+
+        public when_using_as_xunit_class_fixture(PgFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact]
+        public async Task should_create_new_database_and_allow_exec_query_1()
+        {
+            // PREPARE
+            var dbName = "test_xunit_db_1";
+
+            var cmdBuilder = new StringBuilder();
+            cmdBuilder.AppendLine($"CREATE DATABASE {dbName}");
+            cmdBuilder.AppendLine("CONNECTION LIMIT = -1");
+
+            // RUN
+            using (var conn = new Npgsql.NpgsqlConnection(_fixture.ConnectionString))
+            {
+                await conn.OpenAsync();
+                var cmd = new NpgsqlCommand(cmdBuilder.ToString(), conn);
+                cmd.ExecuteNonQuery();
+
+                // ASSERT
+                var dbExists = await new NpgsqlCommand($"SELECT datname FROM pg_catalog.pg_database WHERE datname = '{dbName}'",conn)
+                    .ExecuteScalarAsync();
+
+                Assert.NotNull(dbExists);
+            }
+        }
+
+        [Fact]
+        public async Task should_create_new_database_and_allow_exec_query_2()
+        {
+            // PREPARE
+            var dbName = "test_xunit_db_2";
+
+            var cmdBuilder = new StringBuilder();
+            cmdBuilder.AppendLine($"CREATE DATABASE {dbName}");
+            cmdBuilder.AppendLine("CONNECTION LIMIT = -1");
+
+            // RUN
+            using (var conn = new Npgsql.NpgsqlConnection(_fixture.ConnectionString))
+            {
+                await conn.OpenAsync();
+                var cmd = new NpgsqlCommand(cmdBuilder.ToString(), conn);
+                cmd.ExecuteNonQuery();
+
+                // ASSERT
+                var dbExists = await new NpgsqlCommand($"SELECT datname FROM pg_catalog.pg_database WHERE datname = '{dbName}'",conn)
+                    .ExecuteScalarAsync();
+
+                Assert.NotNull(dbExists);
+            }
+        }
+    }
+}
